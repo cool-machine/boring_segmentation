b4cf181a3519730b743b4da80e211cb86fc7d5fd
Cool Machine <cool.lstm@gmail.com>
2025-03-31 22:09:35 +0200

    necessary files for azure

---
+++ app.py
@@ -0,0 +1,13 @@
+# In app.py at the root
+import os
+import subprocess
+import sys
+
+if __name__ == "__main__":
+    port = os.environ.get("PORT", "8501")
+    subprocess.call([
+        "streamlit", "run", "streamlit_run.py",  # Note the path
+        "--server.port", port,
+        "--server.address", "0.0.0.0",
+        "--server.enableCORS", "false"
+    ])
