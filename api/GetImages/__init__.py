f0d1fa97531904efea200d76aadaf9db55b11931
Cool Machine <cool.lstm@gmail.com>
2025-04-07 01:11:44 +0200

    forced commit

--- api/GetImages/__init__.py
+++ api/GetImages/__init__.py
@@ -4,7 +4,6 @@ import logging
 import azure.functions as func
 import json
 import os
-import subprocess
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request to get images.')
